b9d07c0fa39fc1e79dea1a7cf5192726ab58d869
Rafael Rocha da Silva Proença <rafael.proenca@compasso.com.br>
2025-02-01 14:29:34 -0300

    realizar as finalizadçoes

--- app/src/main/java/br/edu/utfpr/rafaelproenca/aroma_library/AromaLibraryActivity.java
+++ app/src/main/java/br/edu/utfpr/rafaelproenca/aroma_library/AromaLibraryActivity.java
@@ -14,7 +14,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import java.util.ArrayList;
 
 public class AromaLibraryActivity extends AppCompatActivity {
-
+//adicionar RadioGroup de genero: masculino, feminino, unissex
+//adicionar RadioGroup de período: noturno, diurno, qualquer hora
+//adicionar 3 EditText para a pirâmide olfativa (notas de saída, notas de corpo e notas de fundo)
     private EditText editTextAroma;
     private CheckBox checkBoxFavoritos;
     private RadioGroup radioGroupLongevidade, radioGroupProjecao;
